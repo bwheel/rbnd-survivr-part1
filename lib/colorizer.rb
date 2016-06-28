70801b115ac67d6eaa9f95ad6a01ae271c57447c
Byron Wheeler <bwheel1990@gmail.com>
2016-06-27 22:18:14 -0500

    got the color functions to dynamically create

--- lib/colorizer.rb
+++ lib/colorizer.rb
@@ -1,25 +1,36 @@
 class String
   
-    @colors = {
-        :red => 31, 
-        :green => 32, 
-        :yellow => 33, 
-        :blue => 34, 
-        :pink => 35, 
-        :light_blue => 36, 
-        :white => 97, 
-        :light_grey => 37, 
-        :black => 30
-      }
-  
-
 # this function should create a new function for each color.
-  def create_colors
-    @colors.each do |key_color, value|
-      class_eval("def self.{color}; return \"\e[\" << {value} << to_s << \"\e[0m]\"    ; end; ")
+  def self.create_colors
+    colors = {
+      :red=> 31, 
+      :green=> 32, 
+      :yellow=> 33, 
+      :blue=> 34, 
+      :pink=> 35, 
+      :light_blue=> 36, 
+      :white=> 97, 
+      :light_grey=> 37, 
+      :black=> 30
+    }
+
+    colors.each do |key_color, value|
+
+       color_func = %Q{
+        def #{key_color}
+          return "\e[#{value}m\#{self}\e[0m"
+        end
+      }
+      String.class_eval(color_func)
+      #String.class_eval do
+      #  def key_color
+      #    return "\e[#{value}m\#{self}\e[0m"
+      #  end
+      #end
     end
   end
 
+  
   def self.colors
     @colors.keys
   end
@@ -31,9 +42,8 @@ class String
     end
   end
 end
-
-temp = "test"
-valuex = 55
-colorx = :red
-
-puts "def #{colorx}; return \e[#{valuex} \"#{temp.to_s}\"\e[0m]\"    ; end;" 
+String.create_colors
+puts "test".red
+puts "test".green
+puts "test".blue
+puts "test".light_grey
